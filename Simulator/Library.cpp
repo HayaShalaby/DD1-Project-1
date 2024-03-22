0c115d069818cfeea98bc6e471421153a6906ed2
Haya Shalaby <hayashalaby@aucegypt.edu>
2024-03-22 02:16:21 +0200

    Adding comments

--- Simulator/Library.cpp
+++ Simulator/Library.cpp
@@ -3,13 +3,9 @@
 #include <stack>
 #include <sstream>
 
-//static vector<Gate> libComps;
-
 Library::Library(string libFile, Circuit * circuit) {
-
     currCirc = circuit;
     readLib(libFile);
-    thisLibFile = libFile;
 }
 
 Library::~Library() {};
@@ -17,11 +13,10 @@ Library::~Library() {};
 void Library::readLib(string libFile) {
     string line, name, opr, IS, D;
     int inputSize, delay;
-    //    vector<Gate> libComps;
     ifstream inFile(libFile);
 
+    //Parsing .lib file line by line into gate attributes
     while (getline(inFile, line)) {
-        //        getline(inFile, line);
         stringstream ss(line);
         getline(ss, name, ',');
         getline(ss, IS, ',');
@@ -30,6 +25,7 @@ void Library::readLib(string libFile) {
         getline(ss, D, ',');
         delay = stoi(D);
 
+        //Creating a Gate object & adding it to the vector that stores the library components
         Gate g(name, inputSize, opr, delay);
         libComps.push_back(g);
     }
@@ -38,6 +34,7 @@ void Library::readLib(string libFile) {
 }
 
 int Library::getDelay(string gateType) {
+    //Looping through the library components until a match to the given gate is found
     for (int i = 0; i < libComps.size(); i++) {
         if (libComps[i].name == gateType)
             return libComps[i].delay;
@@ -46,6 +43,7 @@ int Library::getDelay(string gateType) {
 }
 
 int Library::getInputSize(string gateType) {
+    //Looping through the library components until a match to the given gate is found
     for (int i = 0; i < libComps.size(); i++) {
         if (libComps[i].name == gateType)
             return libComps[i].inputSize;
@@ -54,6 +52,7 @@ int Library::getInputSize(string gateType) {
 }
 
 string Library::getOperation(string gateType) {
+    //Looping through the library components until a match to the given gate is found
     for (int i = 0; i < libComps.size(); i++) {
         if (libComps[i].name == gateType)
             return libComps[i].operation;
@@ -62,15 +61,16 @@ string Library::getOperation(string gateType) {
 }
 
 int Library::precedence(char op) {
+    //NOT has the highest precedence in logic operations, followed by AND and OR
     if (op == '~')
         return 1;
     if (op == '&' || op == '|')
         return 2;
-    //might need to add a third return
     return 0;
 }
 
 int Library::applyOp(bool in1, bool in2, char op) {
+    //Applies the arithmetic operations equivalent to the logic operators
     switch (op) {
     case '&': return in1 * in2;
     case '|': return in1 + in2;
@@ -78,42 +78,39 @@ int Library::applyOp(bool in1, bool in2, char op) {
     return -1;
 }
 
-//should return value be bool or would it not matter??
+
 int Library::operStack(string operation, int inputSize, vector<Signal> inputs) {
+    //Index for loops
     int i;
-    //not sure if should subtract 1 or not
-    //int in = inputSize - 1;
 
-    //index for inputs
-    int in = 0;
-
-    // stack to store bool values.
+    //Stack to store bool values
     stack <bool> values;
 
-    // stack to store operators.
+    //Stack to store operators
     stack <char> ops;
 
     int s = operation.length();
 
+    //This flag is set to true if NOT is applied to an individual input not a bracket, is checked later on when pushing that input onto the 'values' stack
+    //Needed for correct evaluation of the operation expression
     bool notFlag = false;
+
+    //Looping over every character in the operation expression
     for (i = 0; i < operation.length(); i++) {
 
-        // Current token is a whitespace,
-        // skip it.
+        //Current char is a whitespace, skip it
         if (operation[i] == ' ')
             continue;
 
-        // Current token is an opening
-        // brace, push it to 'ops'
+        //Current char is an opening brace, push it to 'ops'
         else if (operation[i] == '(') {
             ops.push(operation[i]);
         }
 
-        // Current token is a number, push
-        // it to stack for numbers.
+        //Current char is 'i', substitute in an input value & push it to 'values'
         else if (operation[i] == 'i') {
             int index = int(operation.at(i + 1) - '0') - 1 ;
-            bool val = inputs[index].value; //sub in input
+            bool val = inputs[index].value;
             if (notFlag)
             {
                 values.push(!val);
@@ -121,24 +118,12 @@ int Library::operStack(string operation, int inputSize, vector<Signal> inputs) {
             }
             else
                 values.push(val);
-            
-            //in++;
-            //in--;
 
-            i++; 
-            // right now the i points to
-            // the character next to the digit,
-            // since the for loop also increases
-            // the i, we would skip one
-            //  token position; we need to
-            // decrease the value of i by 1 to
-            // correct the offset.
-            // corrected by i++ instead of i+=2
-            //i--;
+
+            i++;
         }
 
-        // Closing brace encountered, solve
-        // entire brace.
+        //Closing brace encountered, solve entire brace
         else if (operation[i] == ')')
         {
             while (!ops.empty() && ops.top() != '(')
@@ -164,12 +149,12 @@ int Library::operStack(string operation, int inputSize, vector<Signal> inputs) {
 
             }
 
-            // pop opening brace.
+            //Pop opening brace
             if (!ops.empty())
                 ops.pop();
         }
 
-        // Current token is an operator.
+        //Current token is an operator
         else
         {
             if (operation[i] == '~') {
@@ -180,33 +165,10 @@ int Library::operStack(string operation, int inputSize, vector<Signal> inputs) {
             }
             else
                 ops.push(operation[i]);
-            //trying: pushing operation onto ops stack
-            //ops.push(operation[i]);
-            // While top of 'ops' has same or greater
-            // precedence to current token, which
-            // is an operator. Apply operator on top
-            // of 'ops' to top two elements in values stack.
-           /* while (!ops.empty() && precedence(ops.top())
-                >= precedence(operation[i]) && values.size()>1) {
-                int val2 = values.top();
-                values.pop();
-
-                int val1 = values.top();
-                values.pop();
-
-                char op = ops.top();
-                ops.pop();
-
-                values.push(applyOp(val1, val2, op));
-            }*/
-
-            // Push current token to 'ops'.
         }
     }
 
-    // Entire expression has been parsed at this
-    // point, apply remaining ops to remaining
-    // values.
+    //Entire expression has been parsed at this point, apply remaining ops to remaining values
     while (!ops.empty()) {
         char op = ops.top();
         ops.pop();
@@ -228,50 +190,13 @@ int Library::operStack(string operation, int inputSize, vector<Signal> inputs) {
         }
     }
 
-    // Top of 'values' contains result, return it.
+    //Top of 'values' contains result, return it
     return values.top();
 }
 
-//URE TREATING IT AS A POSTFIX EXPRESSION CHANGE INFIX FIRST TO EVALUATE
-//ADD ~ IN AN IF BEOFRE THE SWITCH?
-//if this character is the first character and also ~ then flag = true and after the brackets are opened and closed flip the result
-//bool Library::operStack(string operation, int inputSize, vector<Signal> inputs){
-//    //ADD PRECEDENCE ESP BRACKETS
-//    stack<string> oper;
-//    bool operand, operand1, operand2, result;
-//
-//
-//
-//    for (int i = 0; i < operation.length(); i++) {
-//        if (operation[i] == 'i') {
-//            operand = operation[i] + operation[i + 1];
-//            oper.push(operand);
-//            i++;
-//        } else {
-//            switch (operation[i]) {
-//                case '&':
-//                    operand1 * operand2;
-//                    //multiply their logic values
-//                    break;
-//                case '|':
-//                    operand1 + operand2;
-//                    //add their logic values
-//                    break;
-//                case '~':
-//                    operand = !operand;
-//                    //if operand1 = 1 then 0 else 1
-//                    break;
-//            }
-//        }
-//
-//    }
-//}
+
 
 bool Library::logicChange(int index) {
-    //string libFile;
-    //removed the lib object
-    //Library lib(thisLibFile, currCirc);
-
     string gateType = currCirc->getType(index);
     vector<Signal> inputs = currCirc->getInputs(index);
     Signal output = currCirc->getOutput(index);
@@ -280,9 +205,6 @@ bool Library::logicChange(int index) {
     int inputSize = getInputSize(gateType);
     bool result = operStack(exp, inputSize, inputs);
 
-    //not needed because of setLogic (initializes them once)
-    /*int delay = getDelay(gateType);
-    currCirc->setDelay(index, delay);*/
 
     Signal changedOut;
     changedOut.name = "C_O";
@@ -297,7 +219,6 @@ bool Library::logicChange(int index) {
 }
 
 void Library::setLogic(int index) {
-    
     string gateType = currCirc->getType(index);
     vector<Signal> inputs = currCirc->getInputs(index);
     Signal output = currCirc->getOutput(index);
@@ -334,11 +255,9 @@ void Library::setLogic(int index) {
                 }
         }
     }
-
     //for every gate
     // for every ouput
     // for every gate
     // if output name same as input name
     // set make the vales equal
-
 }
--- Simulator/Library.h
+++ Simulator/Library.h
@@ -7,34 +7,49 @@
 #include <string>
 using namespace std;
 
+//Struct to create a type Gate and store all gate attributes
 struct Gate {
     string name;
+    //No. of inputs
     int inputSize;
     string operation;
     int delay;
 
+    //Gate struct constructor
+    //Initializes member variables with zeros/empty strings unless values are provided
     Gate(string gateName = "", int inSize = 0, string opr = "", int gateDelay = 0) : name(gateName), inputSize(inSize), operation(opr), delay(gateDelay) {}
 };
 
+
+//Class to create a type Library and manage all .lib file attributes
 class Library {
+    //Vector that will store all the library components
     vector<Gate> libComps;
-    //turned this into a pointer
+    //An object of class Circuit (a circuit)
     Circuit* currCirc;
-    string thisLibFile;
 public:
-    //ADD A CONSTRUCTOR WHICH CALLS READLIB AND POPULATES LIBCOMPS
-    //takes 
+    //Constuctor
+    //Takes the .lib file name and calls the readLib function to read and parse it
     Library(string libFile, Circuit* circuit);
+    //Destructor
     ~Library();
+    //Parses data from .lib file
     void readLib(string libFile);
+    //Getter for a gate's num. of inputs
     int getInputSize(string gateType);
+    //Getter for a gate's delay
     int getDelay(string gateType);
+    //Getter for a gate's operation expression
     string getOperation(string gateType);
+    //Determines precedence of given operator
     int precedence(char op);
+    //Applies given operator on the two given bool values
     int applyOp(bool in1, bool in2, char op);
+    //Evaluates a given gate operation expression and returns result
+    //Uses the functions precedence and applyOp
     int operStack(string operation, int inputSize, vector<Signal> inputs);
-    //    bool operStack(string operation, int inputSize, vector<Signal> inputs);
+    //Uses operStack to update a circuit's logic values and checks if a logic change occured
     bool logicChange(int index);
-    //sets the logic of gates at the beginning
+    //Sets the logic of gates at the beginning
     void setLogic(int index);
 };
